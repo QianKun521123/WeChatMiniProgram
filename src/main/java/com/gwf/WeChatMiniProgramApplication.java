a899f0a58207cc708473904253909ffcc5e413ca
gwf <2278417331@qq.com>
2024-07-22 12:50:50 +0800

    init:基础信息补充完善

---
+++ src/main/java/com/gwf/WeChatMiniProgramApplication.java
@@ -0,0 +1,16 @@
+package com.gwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author gwf
+ * @Data 2024/7/22 下午12:11
+ * 微信小程序启动类
+ **/
+@SpringBootApplication
+public class WeChatMiniProgramApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatMiniProgramApplication.class, args);
+    }
+}
---
+++ src/main/java/com/gwf/config/WeChatMiniProgramConfig.java
@@ -0,0 +1,24 @@
+package com.gwf.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author gwf
+ * @Data 2024/7/22 下午12:42
+ * 项目配置类
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "wmp")
+public class WeChatMiniProgramConfig {
+    /** 项目名称 */
+    private String name;
+
+    /** 版本 */
+    private String version;
+
+    /** 项目创建时间 */
+    private String createTime;
+}
