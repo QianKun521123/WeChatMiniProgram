655f54760bea1b5e0660417435f872fa75b4b263
gwf <2278417331@qq.com>
2024-07-25 09:14:30 +0800

    add:验证码和redis,优化结构和代码,日志log4j2

--- src/main/java/com/gwf/config/redis/RedisConfig.java
+++ src/main/java/com/gwf/config/redis/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.gwf.config.redis;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
