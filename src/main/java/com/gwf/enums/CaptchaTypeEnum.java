5380da39b0df4286fc1c1fa93645fb2c36e9729a
gwf <2278417331@qq.com>
2024-07-26 13:05:03 +0800

    add:验证码和redis,优化结构和代码

---
+++ src/main/java/com/gwf/enums/CaptchaTypeEnum.java
@@ -0,0 +1,26 @@
+package com.gwf.enums;
+
+/**
+ * @Author gwf
+ * @Data 2024/7/26 下午1:01
+ * 验证码类型枚举
+ **/
+public enum CaptchaTypeEnum {
+
+    /**
+     * 圆圈干扰验证码
+     */
+    CIRCLE,
+    /**
+     * GIF验证码
+     */
+    GIF,
+    /**
+     * 干扰线验证码
+     */
+    LINE,
+    /**
+     * 扭曲干扰验证码
+     */
+    SHEAR
+}
--- src/main/java/com/gwf/service/sys/impl/BaseServiceImpl.java
+++ src/main/java/com/gwf/service/sys/impl/BaseServiceImpl.java
@@ -1,10 +1,13 @@
 package com.gwf.service.sys.impl;
 
+import cn.hutool.captcha.AbstractCaptcha;
+import cn.hutool.captcha.CaptchaUtil;
 import cn.hutool.captcha.generator.CodeGenerator;
 import cn.hutool.core.util.IdUtil;
 import com.gwf.config.WeChatMiniProgramConfig;
 import com.gwf.config.captcha.CaptchaProperties;
 import com.gwf.constants.CacheConstants;
+import com.gwf.enums.CaptchaTypeEnum;
 import com.gwf.model.converter.sys.WeChatMiniProgramConfigConverter;
 import com.gwf.model.vo.sys.CaptchaVo;
 import com.gwf.model.vo.sys.WeChatMiniProgramConfigVo;
@@ -12,6 +15,8 @@ import com.gwf.service.sys.BaseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author gwf
@@ -48,9 +53,34 @@ public class BaseServiceImpl implements BaseService {
     @Override
     public CaptchaVo getCaptcha() {
 
-        // 保存验证码信息
-        String uuid = IdUtil.simpleUUID(); // fastSimpleUUID
-        String verifyKey = CacheConstants.CAPTCHA_CODE_KEY + uuid;
-        return null;
+        AbstractCaptcha captcha;
+        if (CaptchaTypeEnum.CIRCLE.name().equalsIgnoreCase(captchaProperties.getType())) {
+            captcha = CaptchaUtil.createCircleCaptcha(captchaProperties.getWidth(), captchaProperties.getHeight(),
+                    captchaProperties.getCode().getLength(), captchaProperties.getInterfereCount());
+        } else if (CaptchaTypeEnum.GIF.name().equalsIgnoreCase(captchaProperties.getType())) {
+            captcha = CaptchaUtil.createGifCaptcha(captchaProperties.getWidth(), captchaProperties.getHeight(),
+                    captchaProperties.getCode().getLength());
+        } else if (CaptchaTypeEnum.LINE.name().equalsIgnoreCase(captchaProperties.getType())) {
+            captcha = CaptchaUtil.createLineCaptcha(captchaProperties.getWidth(), captchaProperties.getHeight(),
+                    captchaProperties.getCode().getLength(), captchaProperties.getInterfereCount());
+        } else if (CaptchaTypeEnum.SHEAR.name().equalsIgnoreCase(captchaProperties.getType())) {
+            captcha = CaptchaUtil.createShearCaptcha(captchaProperties.getWidth(), captchaProperties.getHeight(),
+                    captchaProperties.getCode().getLength(), captchaProperties.getInterfereCount());
+        } else {
+            throw new IllegalArgumentException("Invalid captcha type: " + captchaProperties.getType());
+        }
+        captcha.setGenerator(codeGenerator);
+        captcha.setTextAlpha(captchaProperties.getTextAlpha());
+        String captchaCode = captcha.getCode();
+        String imageBase64Data = captcha.getImageBase64Data();
+
+        String captchaKey = IdUtil.fastSimpleUUID();
+        redisTemplate.opsForValue().set(CacheConstants.CAPTCHA_CODE_KEY + captchaKey, captchaCode,
+                captchaProperties.getExpireSeconds(), TimeUnit.SECONDS);
+        return CaptchaVo.builder()
+                .captchaBase64(imageBase64Data)
+                .captchaKey(captchaKey)
+                .build();
+
     }
 }
